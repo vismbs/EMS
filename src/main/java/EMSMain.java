b57a665dc9b20ccb109cc056a23df071081364cc
nessun-dorma <vismdbs@gmail.com>
2022-01-22 07:50:42 +0530

    Update View_Employees and Added Print_Data

--- src/main/java/EMSMain.java
+++ src/main/java/EMSMain.java
@@ -5,9 +5,23 @@ import java.sql.SQLException;
 public class EMSMain {
     public static void main(String[] args) throws IOException, FontFormatException, SQLException, ClassNotFoundException {
 //        DBConn Conn = new DBConn();
-//        String SQLTxt = "INSERT INTO Login VALUES (1 ,\"Vishal\", \"$hiniGAMI\")";
+//        String SQLTxt = "DELETE FROM Employee";
+////        String SQLTxt = "CREATE TABLE Employee " +
+////                "(empId TEXT NOT NULL," +
+////                "empName TEXT NOT NULL, " +
+////                "empAge TEXT NOT NULL," +
+////                "empAddress TEXT NOT NULL," +
+////                "empEmail TEXT NOT NULL," +
+////                "empPost TEXT NOT NULL," +
+////                "empFather TEXT NOT NULL," +
+////                "empDOB TEXT NOT NULL," +
+////                "empPhone TEXT NOT NULL," +
+////                "empEdu TEXT NOT NULL," +
+////                "empAadhar TEXT NOT NULL" +
+////                ")";
+////        String SQLString = "DROP TABLE Employee";
 //        Conn.statementInst.executeUpdate(SQLTxt);
-        new FrontPage();
+        new View_Employee();
 
     }
 }
