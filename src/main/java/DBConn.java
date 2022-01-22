5293ffe58be031c262b15e7dae098333aea5ec91
nessun-dorma <vismdbs@gmail.com>
2022-01-22 06:43:11 +0530

    FrontPage and LoginPage Updated

--- src/main/java/DBConn.java
+++ src/main/java/DBConn.java
@@ -1,6 +1,7 @@
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 /**
  * Class Name: DBConn
@@ -10,6 +11,7 @@ public class DBConn {
 
     // A STATIC FIELD FOR CONNECTION VARIABLE AS MAKING IT STATIC WOULD MAKE IT COMMON FOR ALL INSTANCES REDUCING MEMORY LOAD
     private static Connection Conn;
+    Statement statementInst;
 
     // HANDLING THE EXCEPTION AND PRINTING THE ERROR IF THE DB FAILS TO CONNECT
     static {
@@ -21,8 +23,9 @@ public class DBConn {
     }
 
     // FINDING THE SQLITE JDBC CLASS
-    DBConn() throws ClassNotFoundException {
+    public DBConn() throws ClassNotFoundException, SQLException {
         Class.forName("org.sqlite.JDBC");
+        statementInst = Conn.createStatement();
     }
 
     // A GETTER FOR CONNECTION
